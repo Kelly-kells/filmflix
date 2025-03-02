c28038e3bc52e758674173d869855f5c62ec5ca6
Kelly-kells <kellskelechi@gmail.com>
2025-03-02 19:35:31 +0100

    search-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
+import { useState } from 'react'
+import Search from "./components/Search"
 
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState()
   return (
     <main >
       <div className="pattern" />
       <div className="wrapper">
         <header>
-          <img src="./header.png" alt="background-image" className="w-5/6 h-100" />
+          <img src="./header.png" alt="background-image" className="w-screen" />
           <h1>Find<span className="text-gradient"> animes</span> you love  </h1>
         </header>
-        <p>search</p>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       </div>
     </main>
   )
---
+++ src/components/Search.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Search = () => {
+
+
+  return (
+    <div className='text-3xl text-white'>
+      search
+    </div>
+  )
+}
+
+export default Search
