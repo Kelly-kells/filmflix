e7cd4ea22ce968e4c487749a72bafb3f442096a6
Kelly-kells <kellskelechi@gmail.com>
2025-03-19 04:42:47 +0100

    setting error message

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,36 @@ import Search from "./components/Search"
 
 const API_BASE_URL = "https://anime-db.p.rapidapi.com/anime";
 
-const API_KEY = import.meta.env.VITE_anime-db_API_KEY;
+const API_KEY = import.meta.env.VITE_anime - db_API_KEY;
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
+
 const App = () => {
+
   const [searchTerm, setSearchTerm] = useState("")
 
-  useEffect (() => {
+  const [errormessage, setErrorMessage] = useState("")
+
+  const fetchAnime = async () => {
+    try{
+
+    } catch (error) {
+      console.error(`error fetching movies: ${error}`)
+      setErrorMessage("Error fetching movies try again later")
+    }
+  }
+
+  useEffect(() => {
     console.log(searchTerm)
   }, [searchTerm])
 
- 
+
   return (
     <main >
       <div className="pattern" />
